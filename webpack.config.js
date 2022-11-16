74d2eb85dbe3bcc502667ab36a5e0448865c8536
YuichiOta <rb19780519@gmail.com>
2022-11-16 09:40:38 +0900

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  mode: 'development',
+  devtool: 'source-map',
+  entry: {
+    main: './src/javascripts/main.js',
+  },
   output: {
-    path: `${__dirname}/public`,
-    filename: "bundle.js",
+    path: path.resolve(__dirname, './public'),
+    filename: 'javascripts/[name]-[contenthash].js',
+    publicPath: '/',
+  },
+  performance: {
+    maxAssetSize: 3000000,
   },
   devServer: {
     static: "./public",
@@ -13,33 +25,33 @@ module.exports = {
   },
   module: {
     rules: [
-      // HTML
+      // typescript
       {
-        test: /\.(html)$/,
-        use: ["html-loader"],
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
       },
-      // CSS
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      //Javascript
+      // javascript
       {
         test: /\.js$/,
-        // node_modulesは対象外
         exclude: /node_modules/,
-        //トランスコンパイラ
-        use: ["babel-loader"],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { 'targets': '> 0.25%, not dead' }],
+                '@babel/preset-react',
+              ],
+            },
+          },
+        ],
       },
-      //Images
-      {
-        test: /\.(jpg|png|gif|svg)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "assets/images/[hash][ext]",
-        },
-      },
-      //Shader
+      // shader
       {
         test: /\.(glsl|vs|fs|vert|frag)$/,
         type: "asset/source",
@@ -47,6 +59,87 @@ module.exports = {
           filename: "assets/images/[hash][ext]",
         },
       },
+      // css|sass|scss
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: false,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [ 'autoprefixer', { grid: true } ],
+                  [ 'cssnano', { preset: 'default' } ],
+                ],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      // png|jpg|jpeg|svg
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name]-[contenthash][ext]',
+        },
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+            },
+          },
+        ],
+      },
+      // pug
+      {
+        test: /\.pug$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './stylesheets/[name]-[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.html',
+      filename: 'index.html',
+    }),
+    // new HtmlWebpackPlugin({
+    //   template: './src/templates/access.pug',
+    //   filename: 'access.html',
+    // }),
+    // new HtmlWebpackPlugin({
+    //   template: './src/templates/members/taro.pug',
+    //   filename: 'members/taro.html',
+    // }),
+    new CleanWebpackPlugin(),
+  ],
 };
